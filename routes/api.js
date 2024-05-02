53b01c024e33852e5cc5c6675daeb5c2131772eb
ADIGA612 <h97abaza4pro@gmail.com>
2024-05-02 15:03:25 +0300

    feat: implement /api/translate route endpoint in routes/api.js

--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,18 @@
 const Translator = require('../components/translator.js');
 
 module.exports = function (app) {
-  
+
   const translator = new Translator();
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      const { text, locale } = req.body;
+      if ([text, locale].every(Boolean)) {
+        let translation = translator.translate(text, locale)
+        res.json({ text, translation })
+      } else {
+        if (text === "") return res.json({ error: "No text to translate" })
+        res.json({ error: "Required field(s) missing" })
+      }
     });
 };
